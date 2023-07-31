058149e124ba0c4f4d7b6d86d9f944cd73520f92
simbld <s_bullado51@hotmail.com>
2023-07-31 10:44:01 +0200

    adding css and javascript to manipulate the DOM

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+let welcome = document.getElementById('welcome');
+let enter = document.getElementById('enter');
+let img = document.getElementById('welcomeImg');
+let choose = document.getElementById('chooseCharacter');
+let pseudo = document.getElementById('pseudo');
 let rulesBtn = document.getElementById('toggleRulesBtn');
 let rules = document.getElementById('rules');
+let wizard = document.getElementById('wizardCharacter');
+let warrior = document.getElementById('warriorCharacter');
 
 function toggleRules() {
   if (rules.style.display === 'block') {
